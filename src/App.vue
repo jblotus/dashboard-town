b6a230bcc2b81cf8587da84a2bad616175a0454f
James Fuller <jblotus@gmail.com>
2017-05-19 21:55:31 +0000

    messing with components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+    
+    <template>
+      <md-toolbar>
+        <h1 class="md-title" style="flex: 1">The Top</h1>
+        <md-input-container md-inline v-if="shouldShowSearch">
+          <label>Inline field</label>
+          <md-input v-model="searchTerm"></md-input>
+          <md-icon v-on:click.native="hideSearch">cancel</md-icon>
+        </md-input-container>
+         <md-button v-else class="md-icon=button">
+           <md-icon v-on:click.native="showSearch">search</md-icon>
+         </md-button>
+      </md-toolbar>
+      <div id="main-content">
+        <router-view></router-view>
+      </div>
+      <md-toolbar><h1 class="md-title">The Bottom</h1></md-toolbar>
+    </template>
   </div>
 </template>
 
+<style scoped>
+  #main-content {
+    padding: 20px;
+  }
+</style>
+
 <script>
+import 'vue-material/dist/vue-material.css'
+
 export default {
-  name: 'app'
+  methods: {
+    hideSearch () {
+      this.searchTerm = null
+      this.shouldShowSearch = false
+    },
+    showSearch () {
+      this.shouldShowSearch = true
+    }
+  },
+  data () {
+    return {
+      shouldShowSearch: false,
+      searchTerm: null
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueMaterial from 'vue-material'
 import App from './App'
 import router from './router'
 
+Vue.use(Vuex)
+Vue.use(VueMaterial)
+
 Vue.config.productionTip = false
+
+const store = require('./store/store.js')
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
